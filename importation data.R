c4ad3a8072303c4911a233f9ace931ee4e0a5409
pouvoirdasha <dariia.haryfullina@ensae.fr>
2024-04-26 12:42:33 +0000

    Translation and correction

--- importation data.R
+++ importation data.R
@@ -4,17 +4,18 @@ library(tseries)
 library(ggplot2)
 
 
-# 2 - Importation de données ----
+# 2 - Import of data ----
 data = as.data.frame(insee::get_insee_idbank("010767678"))
 data[,"DATE"] = as.Date(paste(data[,"TIME_PERIOD"], "-01", sep=""))
 data= data[, c("DATE", "OBS_VALUE")]
 
-# 3 - Stationnarité de la série de base ----
+# 3 - Stationnarity tests for basic series -----
 tseries::adf.test(data[, "OBS_VALUE"]) # p-value = 0.75, pas stationnaire
 tseries::kpss.test(data[, "OBS_VALUE"]) # p-value = 0.01, pas stationnaire
 tseries::pp.test(data[, "OBS_VALUE"]) # p-value = 0.01, stationnaire
 
-# 4 - Différenciation et stationnarité de la série différenciée ----
+
+# 4 - Stationnarity for the differentiated series ----
 data[, "diff1"] <- c(NA, diff(data[, "OBS_VALUE"]))
 
 tseries::adf.test(na.omit(data[, "diff1"])) # p-value = 0.01, stationnaire
@@ -22,34 +23,39 @@ tseries::kpss.test(na.omit(data[, "diff1"])) # p-value = 0.1, limite stationnair
 tseries::pp.test(na.omit(data[, "diff1"])) # p-value = 0.01, stationnaire
 
 
-# 5 - Visualisation -----
+# 5 - Visualization -----
 
-#Avant le traitement 
+#Before treating the series
 ggplot(data, aes(x = DATE, y = OBS_VALUE)) +
   geom_line(color = 'turquoise') + 
   
-  labs(title = "Valeurs observées non traitées de la série indice de la production de bière",
+  labs(title = "Before transformation - index of industrial production of beer manufacturing, France",
        x = "Date",
-       y = "Indice de la production de bière") +
-  scale_x_date(date_breaks = "year", date_labels = "%Y")+
+       y = "Index of industrial production of beer manufacturing") +
+  scale_x_date(date_breaks = "2 years", date_labels = "%Y")+
   theme_minimal() + 
   theme(plot.title = element_text(size = 10, face = 'bold', hjust = 0.5),
         axis.title.x = element_text(size = 10),
         axis.text.x = element_text(size=7, angle=90), 
-        axis.title.y = element_text(size = 10))
+        axis.title.y = element_text(size = 8))
 
-# Après traitement (passage à la première différence)
+# After 1-st order differentiation 
 
 ggplot(data, aes(x = DATE, y = diff1)) +
   geom_line(color = 'orange') + 
   
-  labs(title = "Valeurs observées stationnarisées de la série indice de la production de bière",
+  labs(title = "After transformation - index of industrial production of beer manufacturing, France",
        x = "Date",
-       y = "Indice de la production de bière") +
-  scale_x_date(date_breaks = "year", date_labels = "%Y")+
+       y = "Index of industrial production of beer manufacturing") +
+  scale_x_date(date_breaks = "2 years", date_labels = "%Y")+
   theme_minimal() + 
   theme(plot.title = element_text(size = 10, face = 'bold', hjust = 0.5),
         axis.title.x = element_text(size = 10),
         axis.text.x = element_text(size=7, angle=90), 
-        axis.title.y = element_text(size = 10))
+        axis.title.y = element_text(size = 8))
+
+
+#6 - Picking ARMA 
+
+
 
